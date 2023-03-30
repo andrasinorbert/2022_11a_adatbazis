df6e51c8789d9faf6b98e20a84a04f506d600734
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2023-03-30 12:44:23 +0200

    20230330

--- 20230330/app.py
+++ 20230330/app.py
@@ -11,24 +11,51 @@ defaults={
     "DB_PASSWD":"11a_user"
 }
 
-conn=pymysql.connect(
-    host=defaults["DB_HOST"],
-    port=defaults["DB_PORT"],
-    database=defaults["DB_NAME"],
-    user=defaults["DB_USER"],
-    password=defaults["DB_PASSWD"]
-)
+def connectDB(_host, _port, _database, _user, _password):
+    return pymysql.connect(
+            host=_host,
+            port=_port,
+            database=_database,
+            user=_user,
+            password=_password
+        )
 
-cursor=conn.cursor()
+def insertDB(nev, kor, magassag):
+    conn=connectDB(
+        _host=defaults["DB_HOST"],
+        _port=defaults["DB_PORT"],
+        _database=defaults["DB_NAME"],
+        _user=defaults["DB_USER"],
+        _password=defaults["DB_PASSWD"]
+    )
+    cursor=conn.cursor()
+    sql=f"""INSERT INTO `{defaults['DB_TABLE']}`
+        (`név`, `kor`, `magasság`) VALUES 
+        ('{nev}', '{kor}', '{magassag}');"""
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+    
+def selectDB(oszlopnevek, where=1):
+    conn=connectDB(
+        _host=defaults["DB_HOST"],
+        _port=defaults["DB_PORT"],
+        _database=defaults["DB_NAME"],
+        _user=defaults["DB_USER"],
+        _password=defaults["DB_PASSWD"]
+    )
+    cursor=conn.cursor()
+    sql=f"""SELECT {oszlopnevek}
+            FROM`{defaults['DB_TABLE']}`
+            WHERE {where};"""
+    cursor.execute(sql)
+    rows=cursor.fetchall()
+    conn.close()
+    return rows
 
-nev="Józsi"
-kor=45
-magassag=200
+#insertDB("Sanyi", 14, 150)
 
-sql=f"""INSERT INTO `{defaults['DB_TABLE']}`
-    (`név`, `kor`, `magasság`) VALUES 
-    ('{nev}', '{kor}', '{magassag}');"""
+sorok=selectDB("név")
 
-cursor.execute(sql)
-conn.commit()
-conn.close()
+for i in sorok:
+    print(i)
